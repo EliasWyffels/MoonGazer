9beb83ea08e4415518c9e81731ba5f067c03fcbe
EliasWyffels <55881967+EliasWyffels@users.noreply.github.com>
2020-12-16 17:07:17 +0100

    api veranderd wegens ssl certeficate probleem

--- app.js
+++ app.js
@@ -40,13 +40,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const SetMoon = function(percent)
 {
-percentage.innerHTML = Math.round(percent * 100) + "%";
+percentage.innerHTML = Math.round(percent) + "%";
 
-let shadowRadius        = Math.abs(50-(percent*100));
-let lightMove           = (100-(percent*100))*1;
-let shadowMove          = ((percent*100))*1;
+let shadowRadius        = Math.abs(50-(percent));
+let lightMove           = (100-(percent))*1;
+let shadowMove          = ((percent))*1;
 
-if(percent > 0.5)
+if(percent > 50)
 {
   moon.setAttribute('style',"background: linear-gradient(to bottom,rgba(10,20,30,1) 25%,rgba(20,40,60,1) 100%)");
 }
@@ -62,10 +62,18 @@ shadow.setAttribute('style', "right :"+ "calc( (50% - var(--moon-size)/2) - "+sh
 const getPhase = async function()
 {
     id = Number(new Date(day))/1000|0;
-    const data = await fetch(`https://api.farmsense.net/v1/moonphases/?d=${id}`)
+    const data = await fetch(`https://moonapi.p.rapidapi.com/${id}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "a1d89ce9aemshbf9e450c11e9732p15d45fjsn3bad1f763cb5",
+        "x-rapidapi-host": "moonapi.p.rapidapi.com"
+      }
+    })
     .then(r => r.json())
     .catch(err => console.error('An error occured: ',err));
-    SetMoon(data[0].Illumination)
+    console.log(data)
+    console.log(data.moon.illumination)
+    SetMoon(data.moon.illumination)
 }
 
 const getlocation = async function()
