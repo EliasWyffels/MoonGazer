ee5cc7e436c079642a31fee9a0ccc8d76a9cd94a
EliasWyffels <55881967+EliasWyffels@users.noreply.github.com>
2020-12-16 20:38:27 +0100

    Update app.js

--- script/app.js
+++ script/app.js
@@ -104,7 +104,7 @@ const getName = async function(lat,long)
 
 const getTime = async function(lat,long)
 {
-  date.placeholder = day;
+  date.value = day;
   const data = await fetch(`https://api.ipgeolocation.io/astronomy?apiKey=${API_KEY}&lat=${lat}&long=${long}&date=${day}`)
   .then(r => r.json())
   .catch(err => console.error('An error occured: ',err));
